e9c51b6cd68021eb6c58937ccb38c797e0d90b48
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2025-03-17 19:47:57 +0600

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fullName, country string
+	var age int
+
+	fullName = "Fahim Ahammed Firoz"
+	country = "Bangladesh"
+	age = 23
+
+	fmt.Println("Hello, World!")
+	fmt.Println(fullName, country, age)
+}
